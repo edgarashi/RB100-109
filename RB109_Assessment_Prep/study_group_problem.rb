69b36afa2d707b8150ed0949f3d55685b127be3e
Garashi <garashi@protonmail.ch>
2021-03-15 17:46:18 -0500

    Refactored an answer to a coding problem

--- RB109_Assessment_Prep/study_group_problem.rb
+++ RB109_Assessment_Prep/study_group_problem.rb
@@ -3,20 +3,11 @@ Given three arrays of integers, return the sum of elements that are common in ea
 =end
 
 def common(a,b,c)
-  sum_arr = []
-
-  a.each do |elem|
-    if b.include?(elem) && c.include?(elem)
-      sum_arr << elem
-    end
-  end
-  sum_arr.sum
-
-
+  a.select do |elem|
+    b.include?(elem) && c.include?(elem)
+  end.sum
 end
 
-# because 2 & 3 are common in all 3 arrays
-puts common([1,2,3],[5,3,2],[7,3,2]) == 5
 
-# because 2,2 & 3 are common in the 3 arrays
+puts common([1,2,3],[5,3,2],[7,3,2]) == 5
 puts common([1,2,2,3],[5,3,2,2],[7,3,2,2]) == 7
